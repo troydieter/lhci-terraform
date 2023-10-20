0dcb476f650582e9f16655fe61b817d667f09677
Troy Dieter <troy.dieter@slalom.com>
2023-10-20 11:48:27 -0400

    added route53 record

--- acm.tf
+++ acm.tf
@@ -3,9 +3,9 @@ variable "tld" {
   description = "The top level domain name (such as lhci.com)"
 }
 
-variable "fqdn" {
+variable "subdomain" {
   type        = string
-  description = "Fully qualified domain name (such as test.lhci.com)"
+  description = "The sub-domain value (such as test)"
 }
 
 # variable "cert_san" {
@@ -22,7 +22,7 @@ module "acm" {
   source  = "terraform-aws-modules/acm/aws"
   version = "~> 4.0"
 
-  domain_name = var.fqdn
+  domain_name = "${var.subdomain}.${var.tld}"
   zone_id     = data.aws_route53_zone.selected.id
 
   validation_method = "DNS"
@@ -32,4 +32,23 @@ module "acm" {
   wait_for_validation = true
 
   tags = local.tags
+}
+
+module "records" {
+  source  = "terraform-aws-modules/route53/aws//modules/records"
+  version = "~> 2.0"
+
+  zone_name = data.aws_route53_zone.selected.name
+
+  records = [
+    {
+      name    = var.subdomain
+      type    = "CNAME"
+      ttl = 3600
+      records = [
+        module.alb.lb_dns_name
+      ]
+    }
+  ]
+
 }
